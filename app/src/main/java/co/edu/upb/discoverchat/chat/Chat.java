555213786237e6cf9021d55d9285813245bccc65
Aldo Rincón Mora <aldo.rincon38@gmail.com>
2015-03-30 11:56:20 -0500

    Add Chat.java class

---
+++ app/src/main/java/co/edu/upb/discoverchat/chat/Chat.java
@@ -0,0 +1,18 @@
+package co.edu.upb.discoverchat.chat;
+
+public class Chat {
+    private long id;
+    private String name;
+    private boolean group;
+    private String room_image;
+    private ArrayList<Receiver> receivers;
+
+    public Chat(){
+        receivers = new ArrayList<>();
+    }
+
+    public getId(){
+    	
+    }
+
+}
