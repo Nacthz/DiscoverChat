d41a1e8082c7af2e6a4719ab450f5c82bf343acb
Luis García <luis.garciap@upb.edu.co>
2015-03-30 18:57:26 -0500

    Hacer que funcione

--- app/src/main/java/co/edu/upb/discoverchat/MainActivity.java
+++ app/src/main/java/co/edu/upb/discoverchat/MainActivity.java
@@ -16,30 +16,29 @@ package co.edu.upb.discoverchat;
  * limitations under the License.
  */
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.SearchManager;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-        import java.util.Locale;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
 
-        import android.app.Activity;
-        import android.app.Fragment;
-        import android.app.FragmentManager;
-        import android.app.SearchManager;
-        import android.content.Intent;
-        import android.content.res.Configuration;
-        import android.os.Bundle;
-        import android.support.v4.app.ActionBarDrawerToggle;
-        import android.support.v4.view.GravityCompat;
-        import android.support.v4.widget.DrawerLayout;
-        import android.view.LayoutInflater;
-        import android.view.Menu;
-        import android.view.MenuInflater;
-        import android.view.MenuItem;
-        import android.view.View;
-        import android.view.ViewGroup;
-        import android.widget.AdapterView;
-        import android.widget.ArrayAdapter;
-        import android.widget.ImageView;
-        import android.widget.ListView;
-        import android.widget.Toast;
+import java.util.Locale;
 
 /**
  * This example illustrates a common usage of the DrawerLayout widget
