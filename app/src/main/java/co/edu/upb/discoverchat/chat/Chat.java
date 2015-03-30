b9464cded3b5cb3bcd5c866554492792aadd0776
Aldo Rincón Mora <aldo.rincon38@gmail.com>
2015-03-30 12:15:06 -0500

    Update Chat to a real model

--- app/src/main/java/co/edu/upb/discoverchat/chat/Chat.java
+++ app/src/main/java/co/edu/upb/discoverchat/chat/Chat.java
@@ -1,18 +1,40 @@
 package co.edu.upb.discoverchat.chat;
+import java.util.ArrayList;
+
+import  co.edu.upb.discoverchat.models.Receiver;
 
 public class Chat {
     private long id;
     private String name;
     private boolean group;
-    private String room_image;
+    private String roomImagePath;
     private ArrayList<Receiver> receivers;
 
     public Chat(){
         receivers = new ArrayList<>();
     }
 
-    public getId(){
-    	
+    public long getId(){
+    	return id;
+    }
+    public Chat setId(long id){
+    	this.id = id;
+    }
+    public String getName(){
+    	return name;
+    }
+    public Chat setName(String name){
+    	this.name = name;
+    }
+    public String getRoomImagePath(){
+    	return roomImagePath;
+    }
+    public Chat setId(String roomImagePath){
+    	this.roomImagePath = roomImagePath;
+    }
+    public ArrayList<Receiver> getReceivers(){ return receivers; }
+    public Chat setReceivers(ArrayList receivers){
+    	this.receivers = receivers;
     }
 
 }
---
+++ app/src/main/java/co/edu/upb/discoverchat/models/Receiver.java
@@ -0,0 +1,4 @@
+package co.edu.upb.discoverchat.models;
+
+public class Receiver {
+}
