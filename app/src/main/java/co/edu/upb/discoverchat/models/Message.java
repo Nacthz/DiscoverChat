df863613e31fb7b08b12161c527834a26ed6db28
Aldo Rincón Mora <aldo.rincon38@gmail.com>
2015-04-03 10:47:17 -0500

    fixed message class

--- app/src/main/java/co/edu/upb/discoverchat/models/Message.java
+++ app/src/main/java/co/edu/upb/discoverchat/models/Message.java
@@ -1,41 +1,9 @@
 package co.edu.upb.discoverchat.models;
 
-<<<<<<< HEAD
-public class Message{
-
-    private String txt;
-    private Long id;
-    private Boolean itsMine;
-
-    public Message(Long id, String txt, Boolean type) {
-        super();
-        this.id = id;
-        this.txt = txt;
-        this.itsMine = type;
-    }
-
-    public Boolean itsMine(){
-        return itsMine;
-    }
-
-    public String getTxt() { return txt; }
-    public void setName(String txt) {
-        this.txt = txt;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-=======
 import android.database.Cursor;
 
 import org.json.JSONObject;
 
-/**
- * Created by hatsumora on 2/04/15.
- */
 public abstract class Message extends Model{
     public enum Type{
         TEXT,
@@ -58,5 +26,4 @@ public abstract class Message extends Model{
     }
 
     public abstract Type getType();
->>>>>>> origin/master
 }
--- app/src/main/java/co/edu/upb/discoverchat/models/TextMessage.java
+++ app/src/main/java/co/edu/upb/discoverchat/models/TextMessage.java
@@ -4,13 +4,23 @@ package co.edu.upb.discoverchat.models;
  * Created by hatsumora on 2/04/15.
  * This is the text incoming and outcoming message model
  */
-//public class TextMessage extends Message {
+public class TextMessage extends Message {
 
-   // public TextMessage(){
+    private String content;
+    public TextMessage(){
 
-    //}
-    //@Override
-   // public Type getType() {
-   //     return Type.TEXT;
-   //}
-//}
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public Type getType() {
+        return Type.TEXT;
+    }
+}
