139904b622cb20b2af59a74328e828ca6bc6d058
Luis García <luis.garciap@upb.edu.co>
2015-04-10 19:44:24 -0500

    Dark icon group

--- app/src/main/java/co/edu/upb/discoverchat/views/navigation/NavigationDrawerFragment.java
+++ app/src/main/java/co/edu/upb/discoverchat/views/navigation/NavigationDrawerFragment.java
@@ -125,7 +125,7 @@ public class NavigationDrawerFragment extends Fragment implements NavigationDraw
         List<NavigationItem> items = new ArrayList<NavigationItem>();
         items.add(new NavigationItem(getString(R.string.chats), getResources().getDrawable(R.drawable.ic_action_chat)));
         items.add(new NavigationItem(getString(R.string.settings), getResources().getDrawable(R.drawable.ic_action_settings)));
-        items.add(new NavigationItem(getString(R.string.contacts), getResources().getDrawable(R.drawable.ic_action_group)));
+        items.add(new NavigationItem(getString(R.string.contacts), getResources().getDrawable(R.drawable.ic_action_group_dark)));
         return items;
     }
 
