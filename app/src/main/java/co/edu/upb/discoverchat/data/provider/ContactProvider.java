ede821760059c1d0676d47c0b5be3385164ca7ce
Aldo Rincón Mora <aldo.rincon38@gmail.com>
2015-04-10 19:07:29 -0500

    Fixed some performace

--- app/src/main/java/co/edu/upb/discoverchat/data/provider/ContactProvider.java
+++ app/src/main/java/co/edu/upb/discoverchat/data/provider/ContactProvider.java
@@ -20,42 +20,12 @@ public class ContactProvider {
     Context context;
     public Cursor getAllContacts(){
 
-        /*ContentResolver resolver = context.getContentResolver();
-
-        Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
-        String[] projection    = new String[] {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
-                ContactsContract.CommonDataKinds.Phone.NUMBER};
-
-        Cursor c = resolver.query(uri, projection, null, null, null);
-        //int indexName = c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-        //int indexNumber = c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-
-        return c;*/
         Uri uri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;
         String[] projection    = new String[] {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
                 ContactsContract.CommonDataKinds.Phone.NUMBER};
 
         Cursor c = context.getContentResolver().query(uri, projection, null, null, null);
 
-        int indexName = c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-        int indexNumber = c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-        int cntRgs = c.getCount();
-        String[] arr = new String [cntRgs+1];
-        int i=0;
-
-
-
-        if (c.moveToFirst()) {
-            do {
-                if(c.getString(indexNumber).substring(0,1).equals("+")){
-                    arr[i] = c.getString(indexName)+"\n"+c.getString(indexNumber).substring(3);
-                }
-                else{
-                    arr[i] = c.getString(indexName)+"\n"+c.getString(indexNumber);
-                }
-                i++;
-            } while(c.moveToNext());
-        }
         return c;
     }
 
